8edafa252ad300a4876f20f9a85ca346c1a28257
Olga Svalova <harlozavr@gmail.com>
2022-12-20 22:45:00 +0200

    timer spec actually checks timer now

---
+++ cypress/e2e/initial-state.cy.ts
@@ -0,0 +1,12 @@
+describe('On game start', function() {
+    it('should have 00:00 timer, mode "Easy", and 45 cells filled', function() {
+        cy.visit('/');
+        cy.clock();
+        cy.get('.status__time')
+            .should('have.text', '00:00');
+        cy.get('.game__cell--filled')
+            .should('have.length', 45);
+        cy.get('[name="status__difficulty-select"]')
+            .should('have.value', 'Easy');
+    });
+})
--- cypress/e2e/timer.cy.ts
+++ cypress/e2e/timer.cy.ts
@@ -1,12 +1,9 @@
-describe('On game start', function() {
-    it('should have 00:00 timer, mode "Easy", and 45 cells filled', function() {
+describe('Timer', function() {
+    it('ticks', function() {
         cy.visit('/');
-        cy.clock();
-        cy.get('.status__time')
-            .should('have.text', '00:00');
-        cy.get('.game__cell--filled')
-            .should('have.length', 45);
-        cy.get('[name="status__difficulty-select"]')
-            .should('have.value', 'Easy');
+        for (let i = 1; i <= 10; i++) {
+            const timerString = (seconds: number) => seconds < 10 ? `00:0${seconds}` : `00:${seconds}`;
+            cy.contains('.status__time', timerString(i));
+        }
     });
 })
